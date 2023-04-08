66d848b5405457704e8411865b3f7812b8d43262
yshmarov <yshmarov@gmail.com>
2023-04-08 17:57:50 +0200

    test omniauth login

--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -6,7 +6,22 @@ class StaticControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get dashboard" do
+  test "unauthenticated user should not get dashboard" do
+    get static_dashboard_url
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "authenticated github user should get dashboard" do
+    login_with_github
+    post user_github_omniauth_callback_path
+    get static_dashboard_url
+    assert_response :success
+  end
+
+  test "authenticated microsoft user should get dashboard" do
+    login_with_microsoft
+    post user_azure_activedirectory_v2_omniauth_callback_path
     get static_dashboard_url
     assert_response :success
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,19 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def login_with_github
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(Faker::Omniauth.github)
+    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:github]
+  end
+
+  def login_with_microsoft
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:azure_activedirectory_v2] = OmniAuth::AuthHash.new({
+      provider: "azure_activedirectory_v2",
+      uid: '12345',
+      info: { email: Faker::Internet.email }
+    })
+    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:azure_activedirectory_v2]
+  end
 end
